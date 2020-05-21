ccabd1897e38a4e7dea428dfa08dd0234c352486
Angelg10 <angelguzman97@gmail.com>
2020-05-21 11:17:34 -0400

    fetching and displaying smurf

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,14 +1,43 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { fetchSmurf, postSmurf, } from '../store/actions';
 
-const Smurf = () => {
+const Smurf = ({ isFetching, fetchSmurf, Smurfs }) => {
+
+    useEffect(() => {
+        fetchSmurf();
+    }, [fetchSmurf]);
+
     return (
         <div>
-            <h2>Name: </h2>
-            <h2>Age: </h2>
-            <h2>Height: </h2>
+            {isFetching && <h2>Fetching Smurf..</h2>}
+            {!isFetching && Smurfs && (
+                Smurfs.map(smurf => (
+                    <div>
+                        <h2>Name: {smurf.name}</h2>
+                        <h2>Age: {smurf.age}</h2>
+                        <h2>Height: {smurf.height}</h2>
+                    </div>
+                ))
+                )}
         </div>
     )
 }
 
-export default Smurf;
+const mapStateToProps = state => {
+    // console.log(state);
+    return {
+        isFetching: state.isFetching,
+        NewSmurf: {
+            name: state.NewSmurf.name,
+            age: state.NewSmurf.age,
+            height: state.NewSmurf.height,
+        },
+        Smurfs: state.Smurfs
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    { fetchSmurf }
+)(Smurf);
--- smurfs/src/store/reducers/index.js
+++ smurfs/src/store/reducers/index.js
@@ -1,3 +1,4 @@
+import { FETCH_SMURF_START, FETCH_SMURF_SUCCESS }  from '../actions';
 
 const initialState = {
     isFetching: false,
@@ -18,9 +19,17 @@ const initialState = {
 
 export const reducer = (state = initialState, action) => {
     switch(action.type){
-
-    
-    
+        case FETCH_SMURF_START:
+            return {
+                ...state,
+                    isFetching: true,
+            }
+        case FETCH_SMURF_SUCCESS:
+            return {
+                ...state,
+                    isFetching: false,
+                    Smurfs: [action.payload]
+            }
         default:
             return state;
     }
